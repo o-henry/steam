5f13798869cba37eae738b714dd882a6319ab62c
henry <cksgod16@gmail.com>
2022-02-24 21:04:15 +0900

    update user repository - create user

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -3,10 +3,29 @@ import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto } from './dto/create-user.request.dto';
 import { User } from './user.entity';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async create_user(create_user_dto: CreateUserDto): Promise<void> {
     const { username, password } = create_user_dto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
