2b5f6a3ade450aabe5a71c56471664a377c1d198
henry <cksgod16@gmail.com>
2022-02-25 00:00:46 +0900

    fix: synchronize: true related db-relation error, if relation doesn't exist the option must be true

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,8 +6,7 @@ export const OrmConfig: TypeOrmModuleOptions = {
   port: 5432,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
-  database: 'steam-service',
+  database: 'steam',
   autoLoadEntities: true,
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // do not use production -- db is always initialized.
 };
