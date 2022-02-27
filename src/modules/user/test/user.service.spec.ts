522f4bd6528a37f7d9c4bc572ede440e06a64898
henry <cksgod16@gmail.com>
2022-02-27 22:39:23 +0900

    add: fail test - user.service

--- src/modules/user/test/user.service.spec.ts
+++ src/modules/user/test/user.service.spec.ts
@@ -1,12 +1,31 @@
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '../user.service';
+import { UserRepository } from '../user.repository';
 
+const mockUserRepository = () => ({
+  findOne: jest.fn(),
+});
 describe('The user service', () => {
   let user_service: UserService;
-  let find_one: jest.Mock;
+  let user_repository;
 
   beforeEach(async () => {
-    find_one = jest.fn();
-    const moudle = await Test.createTestingModule({});
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: UserRepository,
+          useFactory: mockUserRepository,
+        },
+      ],
+    }).compile();
+
+    user_service = module.get(UserService);
+    user_repository = module.get(UserRepository);
+  });
+  describe('get_user', () => {
+    it('should return the user', async () => {});
+
+    it('should fail on exception', async () => {});
   });
 });
