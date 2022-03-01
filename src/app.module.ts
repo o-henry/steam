871b79857f3add016abd2f6631ebd0b3fc013725
henry <cksgod16@gmail.com>
2022-03-01 21:30:59 +0900

    fix: dotenv to config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
+import { PgOrmConfig } from './config/orm-pg.config';
+import { SqlOrmConfig } from './config/orm-sql.config';
 import { GameModule } from './modules/game/game.module';
 import { UserModule } from './modules/user/user.module';
 import { HealthModule } from './modules/health/health.module';
-import { OrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'prod'
+          ? 'env.prod'
+          : process.env.NODE_ENV === 'stage'
+          ? '.env.stage'
+          : '.env.dev',
+    }),
+    TypeOrmModule.forRoot(SqlOrmConfig()),
+    // TypeOrmModule.forRoot(PgOrmConfig),
     UserModule,
     GameModule,
-    TypeOrmModule.forRoot(OrmConfig),
     HealthModule,
   ],
   controllers: [],
