7884158b2c3a2bc1d92417f801e25cea0176cea8
henry <cksgod16@gmail.com>
2022-04-07 19:29:37 +0900

    fix..

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
-import { PgOrmConfig } from './config/orm-pg.config';
+import { PgOrmConfig } from './config/orm.config';
 import { GameModule } from './modules/game/game.module';
 import { UserModule } from './modules/user/user.module';
 import { HealthModule } from './modules/health/health.module';
